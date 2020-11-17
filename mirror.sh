1a116ff72462ad4acf8387476de7ef00d84a39c3
chenlianghong <clh021@gmail.com>
2020-11-17 16:51:40 +0800

    add mirror.sh

---
+++ mirror.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+lsb_dist=$( get_distribution )
+lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+timefix=$(date +%Y%m%d_%H%M%S)
+if is_wsl; then
+    echo "WSL DETECTED: OH No!"
+    exit
+fi
+
+case "$lsb_dist" in
+    ubuntu)
+        sudo cp /etc/apt/sources.list /etc/apt/sources.list.$timefix.bak
+        sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+    ;;
+
+    debian)
+        sudo cp /etc/apt/sources.list /etc/apt/sources.list.$timefix.bak
+        sudo sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+        sudo sed -i 's|security.debian.org/debian-security|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list
+        apt update; apt install -y apt-transport-https;
+    ;;
+
+    centos|rhel)
+        echo "Centos DETECTED: will be support!"
+        exit
+    ;;
+
+    *)
+        echo "$lsb_dist DETECTED: will be support!"
+        exit
+    ;;
+
+esac
