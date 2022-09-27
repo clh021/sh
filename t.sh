583ff35ddd64fa9063931e6fa8ef18c4e2eff017
clh21 <clh021@gmail.com>
2022-09-27 03:01:48 +0000

    jsut set config user
    Signed-off-by: clh21 <clh021@gmail.com>

--- t.sh
+++ t.sh
@@ -10,6 +10,9 @@ else
     git clone --depth=1 https://clh21@gitee.com/perfbenchmark/ui-detect.git
 fi
 
+git config user.email "clh021@gmail.com"
+git config user.name "chenlianghong"
+
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
 
@@ -22,6 +25,3 @@ pip3 install pyinstaller
 
 echo pyinstaller -F report.py
 echo ~/.local/bin/pyinstaller -F report.py
-
-echo git config user.email "clh021@gmail.com"
-echo git config user.name "chenlianghong"
