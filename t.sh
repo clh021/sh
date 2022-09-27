d188e51b5fe3e9cf156de3bb1a559c3c4d5b472d
clh21 <clh021@gmail.com>
2022-09-27 02:29:23 +0000

    add quick script
    Signed-off-by: clh21 <clh021@gmail.com>

---
+++ t.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone --depth=1 https://clh21@gitee.com/perfbenchmark/ui-detect.git
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
+
+echo export GO111MODULE=on
+echo export GOPROXY=https://goproxy.cn
