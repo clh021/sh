a6c385b16441271816dc5beeb6933548915b0b57
clh21 <clh021@gmail.com>
2022-12-28 05:05:38 +0000

    update t.sh.
    Signed-off-by: clh21 <clh021@gmail.com>

--- t.sh
+++ t.sh
@@ -31,14 +31,16 @@ go get github.com/sorenisanerd/gotty
 echo go get github.com/yudai/gotty
 echo go get github.com/sorenisanerd/gotty
 
-#### build host
-ceprei
-| tag | cpu       | os       | arch     | id        | key    | pwd | report | detect | child |
-| --- | --------- | -------- | -------- | --------- | ------ | --- | ------ | ------ | ----- |
-| 1   | 华为麒麟   | kylinV10 |          | 954927413 |        |     |        |        |       |
-| 2   | loongArch | uos      | loong64  | 454092710 | c      |     |        |        |       |
-| 3   | 兆芯       | uos      | amd64    | 442229937 |        |     |        |        |       |
-| 4   | 海芯       | uos      | amd64    | 411133862 |        |     |        |        |       |
-| 5   | 飞腾       | kylinV10 | arm64    | 117396102 | c      |  c  |        |        |       |
-| 6   | 鲲鹏       | uos      | arm64    | 341567454 |        |     |        |        |       |
-| 7   | 龙芯       | uos      | mips64le | 126601258 | v      |     |        |        |       |
+
+ceprei ceprei1 uosuos Uosuos123
+| cpu       | os       | arch     | id        | key    | pwd | report | detect | child |
+| --------- | -------- | -------- | --------- | ------ | --- | ------ | ------ | ----- |
+| 华为麒麟   | kylinV10 |          | 954927413 |        |     |        |        |       |
+| 华为麒麟   | uos      |          | 954927413 |        |     |        |        |       |
+| loongArch | uos      | loong64  | 454092710 | c      |     |        |        |       |
+| 兆芯       | uos      | amd64    | 442229937 |        |     |        |        |       |
+| 海芯       | uos      | amd64    | 411133862 |        |     |        |        |       |
+| 飞腾       | uos      | arm64    | 117396102 |        |  u  |        |        |       |
+| 飞腾       | kylinV10 | arm64    | 117396102 | c      |  c  |        |        |       |
+| 鲲鹏       | uos      | arm64    | 341567454 |        |  c1 |        |        |       |
+| 龙芯       | uos      | mips64le | 126601258 | v      |     |        |        |       |
