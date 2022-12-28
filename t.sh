70856798fc7a1e321fa36d11abb1745fa6ed58e8
clh21 <clh021@gmail.com>
2022-12-28 04:49:37 +0000

    update t.sh.
    Signed-off-by: clh21 <clh021@gmail.com>

--- t.sh
+++ t.sh
@@ -30,3 +30,15 @@ echo ~/.local/bin/pyinstaller -F report.py
 go get github.com/sorenisanerd/gotty
 echo go get github.com/yudai/gotty
 echo go get github.com/sorenisanerd/gotty
+
+#### build host
+ceprei
+| tag | cpu       | os       | arch     | id        | key    | tty | report | detect | child |
+| --- | --------- | -------- | -------- | --------- | ------ | --- | ------ | ------ | ----- |
+| 1   | 华为麒麟   | kylinV10 |          | 954927413 |        |     |        |        |       |
+| 2   | loongArch | uos      | loong64  | 454092710 | c      |     |        |        |       |
+| 3   | 兆芯       | uos      | amd64    | 442229937 |        |     |        |        |       |
+| 4   | 海芯       | uos      | amd64    | 411133862 |        |     |        |        |       |
+| 5   | 飞腾       | uos      | arm64    | 117396102 | v      |     |        |        |       |
+| 6   | 鲲鹏       | uos      | arm64    | 341567454 |        |     |        |        |       |
+| 7   | 龙芯       | uos      | mips64le | 126601258 | v      |     |        |        |       |
