a0c99ed269ac8211372a90fd8702a705530a525f
clh21 <clh021@gmail.com>
2023-02-10 03:17:52 +0000

    pushd update
    Signed-off-by: clh21 <clh021@gmail.com>

--- t.sh
+++ t.sh
@@ -9,10 +9,11 @@ if [ -f "ui-detect/README.md" ]; then
 else
     git clone --depth=1 https://clh21@gitee.com/perfbenchmark/ui-detect.git
     pushd ui-detect
+    git config user.email "clh021@gmail.com"
+    git config user.name "chenlianghong"
+    popd > /dev/null
 fi
 
-git config user.email "clh021@gmail.com"
-git config user.name "chenlianghong"
 
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
