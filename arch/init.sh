443a7b33cc7711270c1109d9b65b88eeab9c42cc
chenlianghong <clh021@gmail.com>
2023-11-24 10:53:32 +0800

    arch: refactor user command to init.sh

---
+++ arch/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# leehom Chen clh021@gmail.com
+
+# install yay
+# 不在安装系统时安装，可能会报错
+wget https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/x86_64/yay-12.1.3-1-x86_64.pkg.tar.zst -O y.zst | pacman -U --noconfirm ./y.zst
+
+# install ohmyzsh
+# 不在安装系统时安装，有确认交互操作
+git clone https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git && cd ohmyzsh/tools && REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git sh install.sh
+
+# install some pkgs
+sudo pacman -S shellcheck zenity
