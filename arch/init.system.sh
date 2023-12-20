b1635f7ee4381984cc1858949775c51ddb7d99e2
chenlianghong <clh021@gmail.com>
2023-12-20 17:50:47 +0800

    arch: update init.system.sh

--- arch/init.system.sh
+++ arch/init.system.sh
@@ -3,7 +3,10 @@
 
 # install yay
 # 不在安装系统时安装，可能会报错
-wget https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/x86_64/yay-12.1.3-1-x86_64.pkg.tar.zst -O y.zst | pacman -U --noconfirm ./y.zst
+wget -c https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/x86_64/yay-12.1.3-1-x86_64.pkg.tar.zst -O y.zst
+sudo pacman -U ./y.zst
+
+yay -S google-chrome
 
 # install ohmyzsh
 # 不在安装系统时安装，有确认交互操作
