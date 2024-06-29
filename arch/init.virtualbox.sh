7c32dddc1a9f16d3ced40f830d7b6234db96c062
clh21 <103163+clh21@user.noreply.gitee.com>
2024-06-29 15:00:25 +0000

    update arch/init.virtualbox.sh.
    Signed-off-by: clh21 <103163+clh21@user.noreply.gitee.com>

--- arch/init.virtualbox.sh
+++ arch/init.virtualbox.sh
@@ -4,7 +4,8 @@
 
 sudo pacman -Syyu
 sudo pacman -Sy --overwrite "*" virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
-sudo gpasswd -a $USER vboxusers # USB 设备
+# sudo gpasswd -a $USER vboxusers # USB 设备
+sudo usermod -a -G vboxusers $USER # USB 设备
 yay -S virtualbox-ext-oracle --noconfirm
 
 modprobe vboxdrv
