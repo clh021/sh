7d52b1045584574f3b3603554c9ad7e324d34baa
chenlianghong <clh021@gmail.com>
2025-08-04 19:49:16 +0800

    fix(ui-detect): correct repository URL and user configuration

--- t.sh
+++ t.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 #wget https://gitee.com/clh21/sh/raw/master/t.sh
 
-
 if [ -f "ui-detect/README.md" ]; then
-    pushd ui-detect > /dev/null
-    git pull
-    popd > /dev/null
+	pushd ui-detect >/dev/null
+	git pull
+	popd >/dev/null
 else
-    git clone --depth=1 https://clh21@gitee.com/perfbenchmark/ui-detect.git
-    pushd ui-detect
-    git config user.email "clh021@gmail.com"
-    git config user.name "chenlianghong"
-    popd > /dev/null
+	git clone --depth=1 https://clh21@gitee.com/clh21/ui-detect.git
+	pushd ui-detect
+	git config user.email "clh021@gmail.com"
+	git config user.name "chenlianghong"
+	popd >/dev/null
 fi
-
 
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
