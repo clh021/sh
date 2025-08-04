8844001cce267413e21a5b4f114198955cc930c9
chenlianghong <clh021@gmail.com>
2025-08-04 19:40:08 +0800

    feat(arch/kde): add script for WPS installation and language setup
    - Create new script `arch/kde/18.wps.sh` for installing WPS Office and setting language to Chinese
    - Add instructions for using `yay` package manager to install WPS Office and related packages
    - Include steps to switch WPS interface language to Chinese in both Chinese and English environments
    - Provide commands to update `LANGUAGE` environment variable and modify configuration files
    - Add explanations and comments for readability and easier maintenance

---
+++ arch/kde/18.wps.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# leehom Chen clh021@gmail.com
+# install wps
+# yay -S wps-office wps-office-mui-zh-cn ttf-wps-fonts # use wps365
+# Linux 英文系统下，切换wps界面语言为中文
+# head -n 5 (wps et wpp wpspdf)
+# head -n 5 `which et`
+# 添加环境变量 LANGUAGE=zh_CN
+# sed -i '2iLANGUAGE=zh_CN' 文件路径
+# sed -i '2iLANGUAGE=zh_CN' `which wps`
+# 或者
+#   nano  ~/.config/Kingsoft/Office.conf
+# 将
+# [General]
+# languages=
+# PersistentStatus=0
+# 修改为
+# [General]
+# languages=zh_CN
+# PersistentStatus=0
+# 没有[General]就添加[General]
